c8288cfef1fcb27ca3babd25e03ea6838d196fe1
RoswellCR <rosbelcr@gmail.com>
2022-08-02 17:43:02 -0400

    testing strings and numbers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
+import React from 'react';
 
-// components
-import Profile from './components/Profile';
-import UserList from './components/UserList';
-
-//CONTEXT
-import UserState from './context-user/User/UserState';
-
-import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function App() {
   return (
     
-        <div className='container p-4'>
-          <h1>Hola Mundo<h/>
+        <div>
+          <h1>Hola Mundo</h1>
         </div>
     
   );
---
+++ src/utils/strings.js
@@ -0,0 +1,12 @@
+
+export const saluda = (name) => {
+  return 'Hola soy ' + name + ' 4'
+}
+
+export const despide = () => {
+  return 'Bye'
+}
+
+export const getId = () => {
+  return '44-123'
+}
